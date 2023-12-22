a90f419f371abe386d0d2838fd81cf1794f5c745
nayeong kim <psi505968@gmail.com>
2023-12-22 17:28:41 +0900

    first commit

---
+++ src/main/java/com/server/gummymurderer/GummyMurdererApplication.java
@@ -0,0 +1,13 @@
+package com.server.gummymurderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GummyMurdererApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GummyMurdererApplication.class, args);
+    }
+
+}
