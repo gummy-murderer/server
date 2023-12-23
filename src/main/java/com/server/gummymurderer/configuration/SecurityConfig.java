0ceb45d24be90ea21636f7cd8541972451775903
nayeong kim <psi505968@gmail.com>
2023-12-23 11:42:19 +0900

    [Feat] : swagger Configuration 작성

---
+++ src/main/java/com/server/gummymurderer/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.server.gummymurderer.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final String[] SWAGGER = {
+            "/swagger-ui/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .and()
+                .build();
+
+
+    }
+}
---
+++ src/main/java/com/server/gummymurderer/configuration/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.server.gummymurderer.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Gummy Murderer App",
+                description = "gummy murderer app api명세",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("gummy murderer API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
