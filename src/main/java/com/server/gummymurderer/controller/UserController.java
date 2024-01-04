fe857e0e127dce8aa26e9f41ee02aacd9354ef56
taegeun <taegeun0525@gmail.com>
2024-01-05 02:33:30 +0900

    [feat] 회원가입 로직 작성

---
+++ src/main/java/com/server/gummymurderer/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.server.gummymurderer.controller;
+
+import com.server.gummymurderer.domain.dto.user.UserJoinRequest;
+import com.server.gummymurderer.domain.dto.user.UserJoinResponse;
+import com.server.gummymurderer.exception.Response;
+import com.server.gummymurderer.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<Response<UserJoinResponse>> create(@RequestBody UserJoinRequest request) {
+
+        UserJoinResponse joinResponse = userService.join(request);
+        return ResponseEntity.ok(Response.success(joinResponse));
+    }
+}
