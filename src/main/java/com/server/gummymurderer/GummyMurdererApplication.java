25765fd744fc0c593a399a9a2b181d26add5fbb4
nayeong kim <psi505968@gmail.com>
2024-01-05 11:57:16 +0900

    [Feat] : WebSocket 통신 코드

--- src/main/java/com/server/gummymurderer/GummyMurdererApplication.java
+++ src/main/java/com/server/gummymurderer/GummyMurdererApplication.java
@@ -2,8 +2,12 @@ package com.server.gummymurderer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 public class GummyMurdererApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/server/gummymurderer/service/ChatService.java
+++ src/main/java/com/server/gummymurderer/service/ChatService.java
@@ -1,19 +1,16 @@
 package com.server.gummymurderer.service;
 
-import com.server.gummymurderer.domain.dto.chat.ChatDto;
+import com.server.gummymurderer.configuration.WebSocketChatHandler;
 import com.server.gummymurderer.domain.dto.chat.ChatSaveRequest;
 import com.server.gummymurderer.domain.dto.chat.ChatSaveResponse;
-import com.server.gummymurderer.domain.dto.chatroom.ChatRoomDto;
 import com.server.gummymurderer.domain.entity.Chat;
-import com.server.gummymurderer.domain.entity.ChatRoom;
+import com.server.gummymurderer.domain.enum_class.ChatRoleType;
 import com.server.gummymurderer.repository.ChatRepository;
 import com.server.gummymurderer.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,12 +18,24 @@ public class ChatService {
 
     private final ChatRepository chatRepository;
     private final ChatRoomRepository chatRoomRepository;
+    private final WebSocketChatHandler chatHandler;  // WebSocketChatHandler 를 사용하기 위해 추가
 
     // 채팅 보내기
     public ChatSaveResponse sendChat(ChatSaveRequest request) {
 
+        request.setRoleTypesBasedOnAI();
         Chat chat = ChatSaveRequest.toEntity(request, LocalDateTime.now());
+
         chatRepository.save(chat);
+
+        // 채팅 수신자의 타입에 따라 다르게 처리
+        if (chat.getReceiverType() == ChatRoleType.AI) {
+            // 수신자가 AI NPC인 경우 AI 서버로 메시지 전송
+            chatHandler.sendMessageToAIServer(chat.getChatContent());
+        } else {
+            // 수신자가 사용자인 경우 Unity 서버로 메시지 전송
+            chatHandler.sendMessageToUnityServer(chat.getChatContent());
+        }
 
         return ChatSaveResponse.of(chat);
     }
