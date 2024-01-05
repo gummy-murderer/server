bf91d0f64daf9ea3983a4f7e677436ad2ffb85cb
nayeong kim <psi505968@gmail.com>
2024-01-05 17:07:21 +0900

    [BugFix] : 사용하지 않는 import 문 삭제

--- src/main/java/com/server/gummymurderer/configuration/SwaggerConfig.java
+++ src/main/java/com/server/gummymurderer/configuration/SwaggerConfig.java
@@ -3,8 +3,6 @@ package com.server.gummymurderer.configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
