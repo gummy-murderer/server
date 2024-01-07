f7a328fec0e121639e3c3f66b63b2002a13832c9
nayeong kim <psi505968@gmail.com>
2024-01-07 22:32:46 +0900

    [Feat] : 유저-ai npc 채팅 api 통신

--- src/main/java/com/server/gummymurderer/domain/dto/chat/ChatSaveRequest.java
+++ src/main/java/com/server/gummymurderer/domain/dto/chat/ChatSaveRequest.java
@@ -18,26 +18,16 @@ public class ChatSaveRequest {
     private String receiver;
     private String chatContent;
     private int chatDay;
-    private ChatRoleType senderType;
-    private ChatRoleType receiverType;
 
-    public void setRoleTypesBasedOnAI() {
-        if (this.sender.equalsIgnoreCase("ai_npc")) {
-            this.senderType = ChatRoleType.AI;
-            this.receiverType = ChatRoleType.USER;
-        } else {
-            this.senderType = ChatRoleType.USER;
-            this.receiverType = ChatRoleType.AI;
-        }
-    }
-
-    public static Chat toEntity(ChatSaveRequest request, LocalDateTime time) {
+    public static Chat toEntity(ChatSaveRequest request, LocalDateTime time,  ChatRoleType senderType, ChatRoleType receiverType) {
         return Chat.builder()
                 .sender(request.sender)
                 .receiver(request.receiver)
                 .chatContent(request.chatContent)
                 .chatDay(request.chatDay)
                 .chatDate(time)
+                .senderType(senderType)
+                .receiverType(receiverType)
                 .build();
 
     }
