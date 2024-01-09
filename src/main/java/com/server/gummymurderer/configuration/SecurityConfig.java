bd7007322ec0b880c902b67e106ba8b208b6b707
nayeong kim <psi505968@gmail.com>
2024-01-09 22:16:07 +0900

    [Feat] : ai npc 별 채팅 리스트 조회

--- src/main/java/com/server/gummymurderer/configuration/SecurityConfig.java
+++ src/main/java/com/server/gummymurderer/configuration/SecurityConfig.java
@@ -39,6 +39,6 @@ public class SecurityConfig {
                 .and()
                 .build();
 
-
     }
+
 }
--- src/main/java/com/server/gummymurderer/repository/ChatRepository.java
+++ src/main/java/com/server/gummymurderer/repository/ChatRepository.java
@@ -2,11 +2,15 @@ package com.server.gummymurderer.repository;
 
 import com.server.gummymurderer.domain.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    Optional<Chat> findByNo(Long no);
+    @Query("SELECT c FROM Chat c where (c.sender = :userName and c.receiver = :aiNpcName) or (c.sender = :aiNpcName and c.receiver = :userName)")
+    //@Query에서 콜론(:) 뒤에 붙은 문자열은 해당 메소드의 파라미터를 참조
+    List<Chat> findAllByUserAndAINpc(String userName, String aiNpcName);
 
 }
