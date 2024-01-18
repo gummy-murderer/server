f8746fc611c3f875b5d5a988516b8468c22ad328
taegeun <taegeun0525@gmail.com>
2024-01-19 08:59:43 +0900

    [Feat] game_scenario Entity 생성
    ManyToOne, JoinColum(name = "game_set_no"

---
+++ src/main/java/com/server/gummymurderer/domain/entity/GameScenario.java
@@ -0,0 +1,49 @@
+package com.server.gummymurderer.domain.entity;
+
+package com.server.gummymurderer.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "game_scenario_tb")
+public class GameScenario extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "senario_no")
+    private long scenarioNo;
+
+    @Column(name = "victim")
+    private String victim;
+
+    @Column(name = "crime_scene")
+    private String crimeScene;
+
+    @Column(name = "method")
+    private String method;
+
+    @Column(name = "witness")
+    private String witness;
+
+    @Column(name = "testimony")
+    private String testimony;
+
+    @Column(name = "body_condition")
+    private String bodyCondition;
+
+    @Column(name = "daily_summary")
+    private String dailySummary;
+
+    @Column(name = "scenario_token")
+    private long scenarioToken;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_set_no")
+    private GameSet gameSet;
+
+}
