32009e9fde96b7abef48f87a769d7f2e7b565da2
nayeong kim <psi505968@gmail.com>
2024-01-24 02:16:40 +0900

    [Feat] : 유저 체크리스트 저장

---
+++ src/main/java/com/server/gummymurderer/domain/dto/gameUserCheckList/CheckListSaveResponse.java
@@ -0,0 +1,15 @@
+package com.server.gummymurderer.domain.dto.gameUserCheckList;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckListSaveResponse {
+
+    private String mark;
+    private String checkJob;
+
+
+}
--- src/main/java/com/server/gummymurderer/repository/GameNpcRepository.java
+++ src/main/java/com/server/gummymurderer/repository/GameNpcRepository.java
@@ -2,6 +2,12 @@ package com.server.gummymurderer.repository;
 
 import com.server.gummymurderer.domain.entity.GameNpc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface GameNpcRepository extends JpaRepository<GameNpc, Long> {
+
+    Optional<GameNpc> findByGameNpcNo(Long gameNpcNo);
 }
