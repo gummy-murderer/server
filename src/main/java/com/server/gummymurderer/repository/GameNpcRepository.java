b0a250e3017db6d8aaf30c52a4025fa36443950f
taegeun <taegeun0525@gmail.com>
2024-01-24 10:43:48 +0900

    [Feat] Game 저장기능 구현

--- src/main/java/com/server/gummymurderer/repository/GameNpcRepository.java
+++ src/main/java/com/server/gummymurderer/repository/GameNpcRepository.java
@@ -3,8 +3,8 @@ package com.server.gummymurderer.repository;
 import com.server.gummymurderer.domain.entity.GameNpc;
 import com.server.gummymurderer.domain.entity.GameSet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GameNpcRepository extends JpaRepository<GameNpc, Long> {
@@ -12,5 +12,6 @@ public interface GameNpcRepository extends JpaRepository<GameNpc, Long> {
     Optional<GameNpc> findByGameNpcNo(Long gameNpcNo);
 
     Optional<GameNpc> findByNpcName(String npcName);
-    List<GameNpc> findAllByGameSet (GameSet gameSet);
+
+    List<GameNpc> findAllByGameSet(GameSet gameSet);
 }
