cc2bfefc87b2af34b5cb8c2d0e66b68aa41dfb8b
TG_Home\taegeun <taegeun0525@gmail.com>
2024-02-05 00:43:42 +0900

    feat : GameAlibi 엔티티생성

---
+++ src/main/java/com/server/gummymurderer/domain/entity/GameAlibi.java
@@ -0,0 +1,28 @@
+package com.server.gummymurderer.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "game_alibi_tb")
+public class GameAlibi {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "alibi_no")
+    private Long alibiNo;
+
+    private String alibi;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "scenario_no")
+    private GameScenario gameScenario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_npc_no")
+    private GameNpc gameNpc;
+}
