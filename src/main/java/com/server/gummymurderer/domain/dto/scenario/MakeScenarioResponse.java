aa01169ba75121f330d7355561e84abe74097627
nayeong kim <psi505968@gmail.com>
2024-02-16 17:27:56 +0900

    [Bugfix] : 시나리오 정보 전송 시 Game npc list 추가 전송

--- src/main/java/com/server/gummymurderer/domain/dto/scenario/MakeScenarioResponse.java
+++ src/main/java/com/server/gummymurderer/domain/dto/scenario/MakeScenarioResponse.java
@@ -1,10 +1,13 @@
 package com.server.gummymurderer.domain.dto.scenario;
 
+import com.server.gummymurderer.domain.dto.gameNpc.GameNpcDTO;
 import com.server.gummymurderer.domain.entity.GameScenario;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,15 +15,17 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MakeScenarioResponse {
 
-
     private String crimeScene;
     private String dailySummary;
     private String victim;
+    private List<GameNpcDTO> gameNpcList;
 
-    public MakeScenarioResponse(GameScenario savedGameScenario) {
-        this.crimeScene = savedGameScenario.getCrimeScene();
-        this.dailySummary = savedGameScenario.getDailySummary();
-        this.victim = savedGameScenario.getVictim();
-    }
-
+   public static MakeScenarioResponse of(GameScenario savedGameScenario, List<GameNpcDTO> gameNpcList) {
+       MakeScenarioResponse response = new MakeScenarioResponse();
+       response.crimeScene = savedGameScenario.getCrimeScene();
+       response.dailySummary = savedGameScenario.getDailySummary();
+       response.victim = savedGameScenario.getVictim();
+       response.gameNpcList = gameNpcList;
+       return response;
+   }
 }
