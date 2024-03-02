343383a7aa001adad55110bc6e5d16275c442ea6
nayeong kim <psi505968@gmail.com>
2024-03-02 11:21:06 +0900

    [Bugfix] : log 추가

--- src/main/java/com/server/gummymurderer/exception/ErrorResponse.java
+++ src/main/java/com/server/gummymurderer/exception/ErrorResponse.java
@@ -3,8 +3,10 @@ package com.server.gummymurderer.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
