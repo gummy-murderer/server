1c9524c38536bf7aea0c2da8659f39f823244921
nayeong kim <psi505968@gmail.com>
2024-04-26 22:05:32 +0900

    [Bugfix] : GCS Signed URL 적용

--- src/main/java/com/server/gummymurderer/service/GCSService.java
+++ src/main/java/com/server/gummymurderer/service/GCSService.java
@@ -1,16 +1,15 @@
 package com.server.gummymurderer.service;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class GCSService {
@@ -33,7 +32,17 @@ public class GCSService {
         // 파일 업로드
         Blob blob = storage.create(blobInfo, file.getInputStream());
 
+        // Signed URL 생성 옵션 설정
+        URL signedUrl = storage.signUrl(
+                BlobInfo.newBuilder(bucketName, blob.getName()).build(),
+                // URL 유효 시간
+                1, TimeUnit.HOURS,
+                // signed URL을 통해 파일을 어떻게 접근할지 HTTP 메소드 정의
+                Storage.SignUrlOption.httpMethod(HttpMethod.GET),
+                // V4 서명 방식 사용
+                Storage.SignUrlOption.withV4Signature());
+
         // 업로드된 파일의 GCS URL 반환
-        return String.format("https://storage.googleapis.com/%s/%s", bucketName, blob.getName());
+        return signedUrl.toString();
     }
 }
