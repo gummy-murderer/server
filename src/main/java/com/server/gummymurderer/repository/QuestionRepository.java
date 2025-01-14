678ec7de8881e8b491b2a4818e7ae505542c6bd3
kny3037 <psi505968@gmail.com>
2025-01-15 02:37:20 +0900

    [Refactor] : Remove unused method and simplify QuestionRepository

--- src/main/java/com/server/gummymurderer/repository/QuestionRepository.java
+++ src/main/java/com/server/gummymurderer/repository/QuestionRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    Optional<Question> findByGameSet_GameSetNoAndNpcNameAndQuestionIndex(Long gameSetNo, String npcName, Integer questionIndex);
-
     Optional<Question> findTopByGameSet_GameSetNoAndNpcNameOrderByCreatedAtDesc(Long gameSetNo, String npcName);
 }
