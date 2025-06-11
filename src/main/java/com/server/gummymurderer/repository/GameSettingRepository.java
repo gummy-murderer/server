a886e51d92535a9a24db2775cee5f114a84375c8
kny3037 <psi505968@gmail.com>
2025-06-11 15:01:41 +0900

    [Feat] : Add user settings with default to login response

--- src/main/java/com/server/gummymurderer/repository/GameSettingRepository.java
+++ src/main/java/com/server/gummymurderer/repository/GameSettingRepository.java
@@ -1,11 +1,15 @@
 package com.server.gummymurderer.repository;
 
 import com.server.gummymurderer.domain.entity.GameSetting;
+import com.server.gummymurderer.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface GameSettingRepository extends JpaRepository<GameSetting, Long> {
 
+    Optional<GameSetting> findByMember(Member member);
 
 }
